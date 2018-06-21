d1fad8bef0a6b844d7ea4dbfff511c25b28bf567
alchemisterix <37701371+alchemisterix@users.noreply.github.com>
2018-06-21 10:17:45 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_disconnect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ind_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(22, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(117, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(22, 71);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(117, 23);
+            this.button_connect.TabIndex = 1;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(235, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button_disconnect
+            // 
+            this.button_disconnect.Location = new System.Drawing.Point(22, 110);
+            this.button_disconnect.Name = "button_disconnect";
+            this.button_disconnect.Size = new System.Drawing.Size(117, 23);
+            this.button_disconnect.TabIndex = 3;
+            this.button_disconnect.Text = "Disconnect";
+            this.button_disconnect.UseVisualStyleBackColor = true;
+            this.button_disconnect.Click += new System.EventHandler(this.button_disconnect_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "O";
+            // 
+            // ind_box
+            // 
+            this.ind_box.BackColor = System.Drawing.Color.Red;
+            this.ind_box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ind_box.Enabled = false;
+            this.ind_box.Location = new System.Drawing.Point(712, 12);
+            this.ind_box.Name = "ind_box";
+            this.ind_box.ReadOnly = true;
+            this.ind_box.Size = new System.Drawing.Size(76, 20);
+            this.ind_box.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ind_box);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_disconnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "DS18B20 Connector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_disconnect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ind_box;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports; // добавил
+using System.Collections;
+using System.Threading;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        SerialPort _serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+
+            button_disconnect.Enabled = false; //disable disconnect button
+
+
+            ArrayList ports = new ArrayList(); //collecting portnames
+
+
+
+            foreach (string s in SerialPort.GetPortNames()) //get all aviliable ports
+            {
+                comboBox1.Items.Add(s);                     //put port name into combox
+                comboBox1.SelectedIndex = 0;                //set default port to first port founded
+                ports.Add(s);                               //put port name into collections
+            }
+
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)      //Connect button presed
+        {
+
+            _serialPort = new SerialPort(comboBox1.Text, 9600);     //lets try to open port
+            _serialPort.Open();                                     //port opening
+            if (_serialPort.IsOpen)                                 //if port open succes
+            {
+                label1.Text = "OPENING PORT";                       //Show message
+                button_connect.Enabled = false;                     //disable connect button
+                button_disconnect.Enabled = true;                   //and enable disconnect button
+            }
+
+
+
+
+
+        }
+
+        private void button_disconnect_Click(object sender, EventArgs e)
+        {
+            
+            if (_serialPort.IsOpen)                                 //if Serial Port opened
+            {
+                label1.Text = "CLOSING PORT";                       //show message in form
+                button_connect.Enabled = true;                      //enable connect button
+                button_disconnect.Enabled = false;                  //disable disconnect button
+                _serialPort.Close();                                //closing port
+                label1.Text = "PORT CLOSED";                        //show message
+            }
+        }
+
+    } 
+}
